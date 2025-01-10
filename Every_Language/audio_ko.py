aa39bce353d53a2df12a25c5f2e41890bb40559c
mlee1016 <michael_lee14@live.com>
2025-01-10 12:54:17 -0700

    Update audio_ko.py

--- Every_Language/audio_ko.py
+++ Every_Language/audio_ko.py
@@ -2,7 +2,7 @@
 ko_polite_present = list()
 for i in range(0,15):
 
-  ko_polite_present.append(fr'C:\Users\Owner\OneDrive\Desktop\css-HTML\open-project\Every_Language\Audio_file_korean_present_polite\chunk{i+15}.mp3')
+  ko_polite_present.append(fr'Every_Language/Audio_file_korean_present_polite/chunk{i+15}.mp3')
 
 
 
@@ -10,7 +10,7 @@ for i in range(0,15):
 ko_polite_useful = list()
 for i in range(0,20):
 
-  ko_polite_useful.append(fr'C:\Users\Owner\OneDrive\Desktop\css-HTML\open-project\Every_Language\Audio_useful_phrases\_korean_useful_{i}.mp3')
+  ko_polite_useful.append(fr'Every_Language/Audio_useful_phrases/_korean_useful_{i}.mp3')
 
 
 

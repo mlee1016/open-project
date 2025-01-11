dddeecebf2047ea4e0e1d91171acd7664e0d1cf0
mlee1016 <michael_lee14@live.com>
2025-01-11 08:29:12 -0700

    Update J_stream_.py

--- Every_Language/J_stream_.py
+++ Every_Language/J_stream_.py
@@ -35,6 +35,7 @@ if options != None or options == "":
   
   df1 = pd.DataFrame(studying_list)
   sol = df1.columns.tolist()
+  studying_list = df1[[sol[0],sol[2]]].to_numpy().tolist()  
   en_ko = df1[[sol[2],sol[0]]].to_numpy().tolist()
     #st.write(*en_ko)
   #else:

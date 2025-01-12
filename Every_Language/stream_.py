c28814dfd9c735203f7418fc3e49a00c0f9832dd
mlee1016 <michael_lee14@live.com>
2025-01-12 08:05:01 -0700

    Update stream_.py

--- Every_Language/stream_.py
+++ Every_Language/stream_.py
@@ -144,7 +144,14 @@ if options != None or options == "":
       #st.write(error)
       #st.write(f"%{(st.session_state.a-error.__len__())/(st.session_state.a-1)*100}")
       
-      st.balloons()
+      st.balloons
+      if st.button("Again"):
+    
+          st.cache_data.clear()
+          st.session_state.e = 0
+          studying_list = []
+    
+      st.rerun()
       #if st.button("again"):
         #st.cache_data.clear()
         #st.session_state.a = 0
